1adf3b68deacccfd2f2d9e0e7cad01686d9d12ad
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-05-07 17:35:38 +0900

    add react

--- src/index.js
+++ src/index.js
@@ -1,16 +1,4 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import React, { createElement } from 'react';
+import { render } from 'react-dom';
 
-import confetti from "canvas-confetti";
-
-const canvas = document.createElement("canvas");
-canvas.width = "960";
-canvas.height = "960";
-document.body.appendChild(canvas);
-
-confetti.create(canvas, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+render(createElement('div', null, 'Hello'), document.getElementById('root'));
